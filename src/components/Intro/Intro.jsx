88089279ed880e2608993bdcf2ccad07023bbd2f
sznsgthb <suzannehoftijzer@gmail.com>
2025-11-30 17:19:47 +0100

    small fix

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect, useRef, useState } from 'react';
 import ScrollAnimation from '../../containers/ScrollAnimation';
 import '../../containers/App.css';
 import './Intro.css'
@@ -30,7 +29,7 @@ function Intro() {
 
                     <ScrollAnimation animation="fadeInUp" delay={4750}>
                         <div className='intro-text'>
-                            <p>As a content creator and full stack web developer i'm excited about crafting colourful, visually rich, yet simple and user-friendly web experiences.</p>
+                            <p>As a content creator and full-stack web developer i'm excited about crafting colourful, visually rich, yet simple and user-friendly web experiences.</p>
                             <p>While I'm still early in my professional journey, I bring a strong foundation in React, a sharp eye for detail, and genuine enthusiasm for building thoughtful digital experiences.</p>
                             <p>I'm driven by the belief that technology and design should serve a purpose — to connect, inspire, to tell a story, or make life just a little easier.</p>
                             <p>On this page, you'll find a selection of the projects I've been working on.</p>
